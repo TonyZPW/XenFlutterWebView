6d650bd0c705ec048bafa693233b32e39d0a7fd5
大魔王 <wx_af1d8e6133834cf894c66a9ff3b6f6d3@git.code.tencent.com>
2021-04-01 11:20:47 +0800

    feat: add load html

--- lib/platform_interface.dart
+++ lib/platform_interface.dart
@@ -317,6 +317,27 @@ abstract class WebViewPlatformController {
     throw UnimplementedError(
         "WebView getScrollY is not implemented on the current platform");
   }
+
+  /// Load html file from local path
+  ///
+  /// `url` must not be null.
+  ///
+  /// Throws an ArgumentError if `url` is not a valid URL string.
+  Future<void> loadAssetHtmlFile(String url) {
+    throw UnimplementedError(
+        "WebView loadAssetHtmlFile is not implemented on the current platform");
+  }
+
+  /// Load html file from local path
+  ///
+  /// `url` must not be null.
+  ///
+  /// Throws an ArgumentError if `url` is not a valid URL string.
+  Future<void> loadLocalHtmlFile(String url) {
+    throw UnimplementedError(
+        "WebView loadLocalHtmlFile is not implemented on the current platform");
+  }
+
 }
 
 /// A single setting for configuring a WebViewPlatform which may be absent.
